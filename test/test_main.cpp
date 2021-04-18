07048a20636a50766d42b5c28f2db0e72784cdbd
TheNotary <no@email.plz>
2021-04-17 22:31:06 -0500

    plans out a test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -19,6 +19,19 @@ void test_led_builtin_pin_number(void) {
     TEST_ASSERT_EQUAL(5, SENSOR_VOLTAGE_REF);
 }
 
+void mega_test(void) {
+  // what do I want to test?
+  // When I call
+  // irrigation.work()
+  // A shit ton of global variables change
+  //   - Sensor values are updated to expectations
+  //   - Irrigation count on plot one goes up
+  //   - Serial is printed to (requires refactor of println)
+  //   - The relay pin for plot 1 is set to "low" and then back to "high"
+  //   - The method delays exactly x milliseconds
+  //   -
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();    // IMPORTANT LINE!
     RUN_TEST(test_led_builtin_pin_number);
